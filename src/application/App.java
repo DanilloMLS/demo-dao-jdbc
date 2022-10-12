270c69591ad986e566631baf4cc5dcef0c4bd497
Danillo Moraes <moraesdanillo10@gmail.com>
2022-10-11 21:50:46 -0300

    SellerDaoJDBC e DAOFactory

--- src/application/App.java
+++ src/application/App.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DAOFactory;
+import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,5 +14,7 @@ public class App {
 
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, department);
         System.out.println(seller);
+
+        SellerDAO sellerDAO = DAOFactory.createSellerDAO();
     }
 }

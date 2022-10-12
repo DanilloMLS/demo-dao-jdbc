2302c3d6b4350f084bc11a767e419876bd46653f
Danillo Moraes <moraesdanillo10@gmail.com>
2022-10-11 21:24:52 -0300

    Classe Seller

--- src/application/App.java
+++ src/application/App.java
@@ -1,10 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Department department = new Department(1, "books");
         System.out.println(department);
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, department);
+        System.out.println(seller);
     }
 }

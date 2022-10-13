ca28afc457c21957a374251a750c1c70c7e36e35
Danillo Moraes <moraesdanillo10@gmail.com>
2022-10-13 20:55:58 -0300

    Implementando findById e arquivos de acesso ao banco

--- src/application/App.java
+++ src/application/App.java
@@ -9,12 +9,14 @@ import model.entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Department department = new Department(1, "books");
-        System.out.println(department);
+        // Department department = new Department(1, "books");
+        // System.out.println(department);
 
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, department);
-        System.out.println(seller);
+        // Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, department);
+        // System.out.println(seller);
 
         SellerDAO sellerDAO = DAOFactory.createSellerDAO();
+        Seller seller = sellerDAO.findById(3);
+        System.out.println(seller);
     }
 }

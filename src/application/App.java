e27373958ac06389c36523148615174134ffa479
Danillo Moraes <moraesdanillo10@gmail.com>
2022-10-13 22:01:49 -0300

    Correção em findById e implementação de findByDepartment

--- src/application/App.java
+++ src/application/App.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DAOFactory;
 import model.dao.SellerDAO;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class App {
@@ -17,5 +20,12 @@ public class App {
         System.out.println("Teste 1: findById seller");
         Seller seller = sellerDAO.findById(3);
         System.out.println(seller);
+
+        System.out.println("Teste 2: findById seller");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDAO.findByDepartment(department);
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
     }
 }
